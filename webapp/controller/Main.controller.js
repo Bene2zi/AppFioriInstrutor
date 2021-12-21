e3164806bc4fa71ad2fe3e5994d8e26c6b60ab8e
Fabricio Beneduzi <fabricio.beneduzi@gmail.com>
2021-12-21 00:41:25 +0000

    Pdf

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -4,12 +4,13 @@ sap.ui.define([
 	"sap/m/MessageBox",
 	"sap/ui/model/Filter",
     "sap/ui/model/FilterOperator", 
-    "sap/m/BusyDialog"
+    "sap/m/BusyDialog",
+    "sap/m/PDFViewer"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,JSONModel,MessageBox,Filter,FilterOperator,BusyDialog) {
+    function (Controller,JSONModel,MessageBox,Filter,FilterOperator,BusyDialog,PDFViewer) {
         "use strict";
 
         return Controller.extend("AppFioriAluno99.appfiorialuno99.controller.Main", {
@@ -19,11 +20,13 @@ sap.ui.define([
                 this.oFieldScreen   = new JSONModel({ btnsVisible : false, btnEditVisible : true , btnEnabled : true });
                 this.oConsModel     = new JSONModel({ CadastroSet : [], aExcluded : []});
                 this.BusyDialog     = new BusyDialog( )              
-                 
-                //this.getView().setModel(this.oCadastroModel,"oTipoModel"); 
-                
+                  
                 this.oConsModel.updateBindings(true);
                 this.oFieldScreen.updateBindings(true);  
+
+                this.oPdfModel = new JSONModel({})
+        	    this.getView().setModel(this.oPdfModel, "oPdfModel");
+
             },
 
             onSearch: function (oEvent) {
@@ -39,10 +42,11 @@ sap.ui.define([
                 if (Sobrenome) aFilter.push(new Filter('Sobrenome', FilterOperator.Contains, Sobrenome))
                 if (DataNasc) aFilter.push(new Filter('DataNascimento', FilterOperator.EQ, DataNasc))
 
-                //this.getView().byId('idCadastroTable').getBinding('items').filter(aFilter)
-                //this.getView().byId('idCadastroTable').updateBindings(true)
+                this.getView().byId('idCadastroTable').getBinding('items').filter(aFilter)
+                this.getView().byId('idCadastroTable').updateBindings(true)
 
-                this.getOwnerComponent().getModel('oModelSet').read("/CadastroSet",{
+                /*
+                this.getOwnerComponent().getModel('oCadastroModel').read("/CadastroSet",{
                     filters: aFilter,
                     sync: true,
                     success:function(oData,oResponse){
@@ -50,14 +54,13 @@ sap.ui.define([
                         this.getView().setModel(this.oCadastroModel,'oCadastroModel')
 
                         this.oCadastroModel.updateBindings(true)
-                        MessageBox.success("Registro carregad com Sucesso");
+                        //MessageBox.success("Registro carregado com Sucesso");
                     }.bind(this),
                     error:function(oData,oResponse){
-                        MessageBox.error("Erro ao gravar o registro");
+                        //MessageBox.error("Erro ao gravar o registro");
                     }.bind(this)
                 })	
-
-
+                */
             },
 
             onAdd : function(oEvent){
@@ -71,6 +74,8 @@ sap.ui.define([
                 this.onOpenDialog()
             },
 
+
+
             onOpenDialog : function(){
                 this._dialogCadastro = sap.ui.xmlfragment("AppFioriAluno99.appfiorialuno99.view.Cadastro", this)
     			if (this.getView().$().closest(".sapUiSizeCompact").length > 0){
@@ -83,6 +88,7 @@ sap.ui.define([
             	
             	this._dialogCadastro.open()
             },
+
 
             onConfirmar : function(oEvent){
 
@@ -166,6 +172,103 @@ sap.ui.define([
                         }.bind(this)
                     });
                 }
+            },
+
+            showPDF: function(oEvent){
+                let oModel = this.getView().getModel('oCadastroModel').getObject( this.getView().byId('idCadastroTable').getSelectedContextPaths()[0])
+                var opdfViewer = new PDFViewer();
+                this.getView().addDependent(opdfViewer);
+                var sServiceURL = this.getOwnerComponent().getModel('oCadastroModel').sServiceUrl;
+                var sSource = sServiceURL + "/DependentesPdfSet('" + oModel.Cpf + "')/$value";
+                opdfViewer.setSource(sSource);
+                opdfViewer.setTitle( oModel.Cpf + " PDF");
+                opdfViewer.open();	
+            },
+
+            onDependentes : function(oEvent){
+                let aSelectedContexts = this.getView().byId('idCadastroTable').getSelectedContextPaths();
+                
+                if(aSelectedContexts.length){
+                    this.oDependenteModel = new sap.ui.model.json.JSONModel({CadastroDependenteSet:[]})
+
+                    let oModel = this.getView().getModel('oCadastroModel').getObject(aSelectedContexts[0])
+                    this.oCadastroModelLine = new sap.ui.model.json.JSONModel(oModel)
+                    this.getView().setModel(this.oCadastroModelLine, 'oCadastroModelLine')
+                    this.oCadastroDependenteModelLine = new sap.ui.model.json.JSONModel({ CpfDependente: '', Nome: '', Sobrenome: '', Email:'', Telefone:'', DataNascimento : new Date()})
+ 
+                    this.getView().setModel(this.oDependenteModel, 'oDependenteModel')
+                    this.getView().setModel(this.oCadastroDependenteModelLine, 'oCadastroDependenteModelLine')
+                    this.oCadastroDependenteModelLine.updateBindings(true)
+                    this.oCadastroModelLine.updateBindings(true)
+                    this.oDependenteModel.updateBindings(true)
+                    this.onChangeBtnEnabled(false)
+                    this.onOpenDependentesDialog()
+                } 
+            },
+
+            onOpenDependentesDialog : function(){
+                this._dialogCadastroDependentes = sap.ui.xmlfragment("AppFioriAluno99.appfiorialuno99.view.Dependentes", this)
+    			if (this.getView().$().closest(".sapUiSizeCompact").length > 0){
+    				this._dialogCadastroDependentes.addStyleClass("sapUiSizeCompact") 
+    			} else { 
+    				this._dialogCadastroDependentes.addStyleClass("sapUiSizeCozy")
+    			}
+            	
+
+            	this.getView().addDependent(this._dialogCadastroDependentes)
+            	
+            	this._dialogCadastroDependentes.open()
+            },
+
+            onCancelarDependentes : function(oEvent){
+                this._dialogCadastroDependentes.close()
+            },
+
+            onAfterCloseDependentes : function(oEvent){
+                this._dialogCadastroDependentes.destroy()
+            },
+
+            onAddDepentende : function(oEvent){
+
+                this.oCadastroDependenteModelLine.getData().Cpf = this.oCadastroModelLine.getData().Cpf
+                this.oDependenteModel.getData().CadastroDependenteSet.push(this.oCadastroDependenteModelLine.getData())
+                this.oDependenteModel.updateBindings(true)
+
+
+                this.oCadastroModelLine = new sap.ui.model.json.JSONModel({ CpfDependente: '', Nome: '', Sobrenome: '', Email:'', Telefone:'', DataNascimento : new Date()})
+                    this.getView().setModel(this.oCadastroDependenteModelLine, 'oCadastroDependenteModelLine')
+                    this.oCadastroDependenteModelLine.updateBindings(true)
+            },
+
+            onSalvarDependente : function(oEvent){
+                this._dialogCadastroDependentes.close()
+            },
+
+
+            onSalvar : function(oEvent){
+
+                let oModelSend = {Cpf : '12345678902', 
+                Nome : 'Joao', 
+                Sobrenome : 'Fernandes', 
+                DataNascimento : new Date(), 
+                CadastroDependenteSet = []}
+
+                oModelSend.CadastroDependenteSet.push({
+                    Cpf: '12346578902', 
+                    CpfDependente: '32165498701', 
+                    Nome: 'Juliano', 
+                    Sobrenome:'Fernandes', 
+                    DataNascimento: new Date() })
+
+                this.getOwnerComponent().getModel('oCadastroModel').create('/CadastroSet',oModelSend,{
+                    success:function(oData,oResponse){
+                        this.getView().byId('idCadastroTable').updateBindings(true)
+                        MessageBox.success("Registro gerado com Sucesso");
+                    }.bind(this),
+                    error:function(oData,oResponse){
+                        MessageBox.error("Erro ao gravar o registro");
+                    }.bind(this)
+                })	
             },
 
             //Final
